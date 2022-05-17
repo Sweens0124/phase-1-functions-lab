4330b5c891e3909554920932412faa66536b0d7d
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-17 16:14:28 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-function distanceFromHqInBlocks(x) {
-    return Math.max(42 - x, x - 42);
+function distanceFromHqInBlocks (x) {
+    return Math.max(42 - x, x - 42)
 }
 
-function distanceFromHqInFeet(x) {
-    return Math.max(42 - x, x - 42) * 264;
+function distanceFromHqInFeet (x) {
+    return Math.max(42 - x, x - 42) * 264
 }
 
-function distanceTravelledInFeet(x, y) {
-    return distanceFromHqInFeet(distanceFromHqInBlocks(y - x));
+function distanceTravelledInFeet (x, y) {
+    return distanceFromHqInFeet(distanceFromHqInBlocks(y - x))
 }
 
+function calculatesFarePrice (x, y) {
+    const distance = distanceTravelledInFeet(x, y)
+    switch (true) {
+        case (distance < 400): return 0
+        case (distance >= 400 && distance <= 2000): return ((distance - 400) * 0.02)
+        case (distance >= 2000 && distance <= 2500): return 25
+        case (distance > 2500): return 'cannot travel that far'
+    }
+}
